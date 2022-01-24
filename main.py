5cea9e8fb9319377707cfe53df59cae6725e9c44
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-24 20:31:50 +0700

    belajar path url

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": {"name":"sakura endo"}}
+    return {"data": {"blog list"}}
 
-@app.get("/about")
-def about():
-    return {'data' : {'about page'}}
+@app.get("/blog/unpublished")
+def unpublished():
+    # fetch unpublished blogs
+    return {'data' : 'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog id
+    return {'data' : id}
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    # fetch comments of blog id
+    return {'data' : {'1','2','3'}}
