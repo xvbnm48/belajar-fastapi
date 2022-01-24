dc9bc8861f0c5aad8451f1d8299333616abe0937
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-25 00:05:08 +0700

    membuat request body dengan class

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from cgitb import text
+from lib2to3.pgen2.token import OP
+from pickletools import anyobject
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -25,3 +29,14 @@ def show(id: int):
 def comments(id, limit = 10):
     # fetch comments of blog id
     return {'data' : {'1','2','3'}}
+
+# requuest body
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+    
+
+@app.post('/blog')
+def create_blog(blog : Blog):
+    return {'data' : f"blog is created with title {blog.title}"}
