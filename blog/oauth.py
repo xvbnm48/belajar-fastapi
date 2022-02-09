39930691cc61704ef5029c5bb14f07526e6563e2
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-02-09 23:31:19 +0700

    membuat jwt token

---
+++ blog/oauth.py
@@ -0,0 +1,16 @@
+from tokenize import Token
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+import Token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    Token.verify_token(token,credentials_exception)
--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from .. import schemas, database, models
+from datetime import datetime, timedelta
+from .. import schemas, database, models , Token
 from sqlalchemy.orm import Session
 from ..hashing import Hash
+
 
 
 router = APIRouter(
@@ -18,4 +20,5 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Incorrect password")
     # generate jwt token
 
-    return user
+    access_token = Token.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
